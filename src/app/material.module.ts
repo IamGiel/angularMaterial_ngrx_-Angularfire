8119a7499a77fc4c0aeccc373e2d66b530dbc961
IamGiel <november14giel@gmail.com>
2020-01-05 10:25:21 -0500

    start explore sidenav

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,9 +4,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatInputModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatIconModule, MatInputModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
+  imports: [MatButtonModule, MatIconModule, MatInputModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule],
+  exports: [MatButtonModule, MatIconModule, MatInputModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule]
 })
 export class MaterialModule {}
